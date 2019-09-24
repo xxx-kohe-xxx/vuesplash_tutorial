922900c33a0fa748fd7880efff7a5c87b5b3d515
xxx-kohe-xxx <kohe.dekita@gmail.com>
2019-09-25 06:26:51 +0900

    認証系

---
+++ resources/js/util.js
@@ -0,0 +1,21 @@
+/**
+ * クッキーの値を取得する
+ * @param {string} searchKey 検索するキー
+ * @return {string} キーに対応する値
+ */
+export function getCookieValue(searchKey) {
+  if(typeof searchKey === 'undefined'){
+    return ''
+  }
+
+  let val = ''
+
+  document.cookie.split(';').forEach(cookie => {
+    const[key, value] = cookie.split('=')
+    if(key === searchKey){
+      return val = value
+    }
+  })
+
+  return val
+}
