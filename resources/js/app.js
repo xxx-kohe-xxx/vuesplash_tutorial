c9ff97a1d739f070c42c6a72373dc70469fa4b81
xxx-kohe-xxx <kohe.dekita@gmail.com>
2019-09-25 06:22:46 +0900

    add components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,19 @@
+import './bootstrap'
+
 import Vue from 'vue'
 // ルーティングの定義をインポートする
 import router from './router'
+// ストアをインポートする
+import store from './store'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
+
+
 
 new Vue({
   el:'#app',
   router,// ルーティングの定義を読み込む
+  store, // vuexストア 
   components:{App},// ルートコンポーネントの使用を宣言
   template:'<App/>'// ルートコンポーネントを描画する
 })
---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar">
+    <RouterLink class="navbar__brand" to="/">
+      Vuesplash
+    </RouterLink>
+    <div class="navbar__menu">
+      <div class="navbar__item" v-if="isLogin">
+        <button class="button">
+          <i class="icon ion-md-add"></i>
+          Submit a photo
+        </button>
+      </div>
+      <span class="navbar__item" v-if="isLogin">
+        {{ username }}
+      </span>
+      <div class="navbar__item" v-else>
+        <RouterLink class="button button--link" to="/login">
+          Login/Register
+        </RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  computed: {
+    isLogin(){
+      return this.$store.getters['auth/check']
+    },
+    username(){
+      return this.$store.getters['auth/username']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
